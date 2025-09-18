8dc2b638377e0fc34629bbd33c025466f881603a
uluocakomer29Omer <uluocakomer29@gmail.com>
2025-09-19 01:43:52 +0300

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace JenkinsTestAPI.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger) 
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)  
         {
             _logger = logger;
         }
